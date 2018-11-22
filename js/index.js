5af13073ecf4627728f4f87aa57e00dc9dd2153b
Mihelle Torres <lic.nancy.torres@gmail.com>
2018-11-22 08:19:09 -0600

    Agregamos un ejemplo de peticiones fetch

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-let x = 10;
-x--;
-console.log(x);
+const SITE = 'https://rickandmortyapi.com/api';
+
+const picame = document.getElementById('button-picame')
+picame.addEventListener('click', async function() {
+
+  // Esto deberia estar en un metodo de una clase al estilo api.characters.getAll()
+  data = await fetch(`${SITE}/character/`).then(function(response) {
+    return response.json();
+  });
+
+  console.log(data);
+})
